3a5606c0092126ed4e3f3ffc5cb7ce91865f6d9b
Audrey Kang <hkang10@gmail.com>
2016-05-03 21:11:24 -0500

    Add data questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "What is your name?"
+name = gets.chomp
+puts "How old are you?"
+age = gets.chomp
+puts "What year were you born?"
+year = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlicbread = gets.chomp
+puts "Would you like to enroll in the company's health insurance?"
+health = gets.chomp
