ea8dc88457dd07bfc2a500b3bf53e6a5df0dd592
Audrey Kang <hkang10@gmail.com>
2016-05-06 15:22:10 -0500

    initial Vampire file commit

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,3 +1,5 @@
+#Creating a survey for vampire employees
+
 require 'date'
 current_year = Date.today.year
 current_year = current_year.to_i
@@ -25,41 +27,3 @@ garlic_bread = gets.chomp
 
 puts "Would you like to enroll in the company's health insurance?"
 health_insurance = gets.chomp
-
-#Testing Data
-
-if age == current_year - year && (garlic_bread == "yes" || health_insurance == "yes")
-                puts "Probably not a vampire."
-end
-
-if age != current_year - year && (garlic_bread == "no" || health_insurance == "no")
-                puts "Probably a vampire."
-end
-
-if age != current_year - year && (garlic_bread == "no" && health_insurance == "no")
-                puts "Almost certainly a vampire"
-end
-
-if name == "Drake Cula"
-                puts "Definitely a vampire"
-elsif name == "Tu Fang"
-                puts "Definitely a vampire"
-else
-                puts "Results inconclusive"
-end
-
-
-#checking for allergies
-
-puts "Please list all your allergies and write 'Done' when you are done listing."
-allergies = gets.chomp.downcase!
-until allergies == "done" || allergies == "sunshine"
-puts "Probably a vampire"
-end
-
-count += 1
-end
-
-#Ending
-
-puts "Actually, nevermind! What do these questions have to do with anything? Let's all be friends."
