6205ca7a03c6e53804149975fb86010eff3c4e6d
Audrey Kang <hkang10@gmail.com>
2016-05-05 19:21:55 -0500

    Update to release 3

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,13 +1,18 @@
 def encrypt(password)
-  i=0
+    i = 0
+
   while i < password.length
-   var = password[i]
-    puts var.next
+      alpha = "abcdefghijklmnopqrstuvwxyz"
+      var = password[i]
+      num = alpha.index(var)
+      num2 = num + 1
+      num3 = num2%26
+      puts alpha[num3]
     i += 1
   end
 end
 
-encrypt("abc")
+encrypt("aaa")
 
 def decrypt(password)
     i = 0
