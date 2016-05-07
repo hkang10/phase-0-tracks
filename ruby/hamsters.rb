a1072c27e3d048838c5260ddd7269656cea84e13
Audrey Kang <hkang10@gmail.com>
2016-05-06 19:12:12 -0500

    Add output of gathered answers

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -8,10 +8,16 @@ puts "What color fur does your hamster have?"
 fur_color = gets.chomp.to_s
 
 puts "Good candidate for adoption? (Enter 'y' for yes and 'n' for no)"
-adoption = gets.chomp.to_s.downcase!
+adoption = gets.chomp.downcase
 
 puts "How old is your hamster? If you don't know just give me an estimate. If you really don't know, just enter '0'"
 age = gets.chomp.to_f
 if age == 0
   age = nil
 end
+
+puts "Your hamster's name is #{hamsters_name}."
+puts "You hamster's level is #{volume}."
+puts "Your hamster's fur color is #{fur_color}."
+puts "Is your hamster a good candidate for adoption? #{adoption}"
+puts "Your hamster is #{age} years old."
