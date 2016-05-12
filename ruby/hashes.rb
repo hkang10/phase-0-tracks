e32b775bd8a0fea56289f9593f3f8314fcb355c5
Audrey Kang <hkang10@gmail.com>
2016-05-11 19:17:53 -0500

    Add user input questions

---
+++ ruby/hashes.rb
@@ -0,0 +1,18 @@
+#New way
+#Retrieving data
+puts "Client's name:"
+name = gets.chomp.capitalize
+puts "Client's age:"
+age = gets.chomp.to_i
+puts "Number of children in Client's household:" num_of_children = gets.chomp.to_i
+puts "Decor Theme: (Modern, Country, Rustic, Contemporary)" theme = gets.chomp.capitalize
+puts "Budget:"
+budget = gets.chomp
+#Creating new hash to store Client's data
+interior_design = { Username: name,
+}
+Age: age,
+Number_of_Children: num_of_children, Decor_Theme: theme,
+Budget: budget
+puts ""
+puts "Client's information:" interior_design.each {|x, y| puts "#{x}: #{y}" }
