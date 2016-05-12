341d074c7253b4342abfb76899f67f6ae2a3b551
Audrey Kang <hkang10@gmail.com>
2016-05-11 19:24:52 -0500

    Add methods to replace with new data

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -4,8 +4,10 @@ puts "Client's name:"
 name = gets.chomp.capitalize
 puts "Client's age:"
 age = gets.chomp.to_i
-puts "Number of children in Client's household:" num_of_children = gets.chomp.to_i
-puts "Decor Theme: (Modern, Country, Rustic, Contemporary)" theme = gets.chomp.capitalize
+puts "Number of children in Client's household:"
+num_of_children = gets.chomp.to_i
+puts "Decor Theme: (Modern, Country, Rustic, Contemporary)"
+theme = gets.chomp.capitalize
 puts "Budget:"
 budget = gets.chomp
 
@@ -13,9 +15,46 @@ budget = gets.chomp
 interior_design = {
   Username: name,
   Age: age,
-  Number_of_Children: num_of_children, Decor_Theme: theme,
+  Number_of_Children: num_of_children,
+  Decor_Theme: theme,
   Budget: budget
 }
 
 puts ""
-puts "Client's information:" interior_design.each {|x, y| puts "#{x}: #{y}" }
+puts "Client's information:"
+interior_design.each {|x, y| puts "#{x}: #{y}" }
+
+#Methods to replace data
+def new_update(interior_design)
+interior_design[:name] = interior_design[:name]
+interior_design[:age] = interior_design[:age]
+interior_design[:num_of_children] = interior_design[:num_of_children]
+interior_design[:theme] = interior_design[:theme]
+interior_design[:budget] = interior_design[:budget]
+return interior_design
+end
+
+def new_data(interior_design)
+puts "What item do you want to change, otherwise type 'none'"
+new_data = gets.chomp.downcase
+if new_data != "none"
+puts "What is the correct information?"
+response = new_data.chomp
+interior_design[response] = gets.chomp
+else
+puts interior_design
+end
+puts ""
+return interior_design
+end
+
+#Calling methods
+updated_hash = interior_design
+updated_hash = new_update(updated_hash)
+puts updated_hash
+new_data(updated_hash)
+updated_hash = new_update(updated_hash)
+puts updated_hash
+puts ""
+puts "Client's information:"
+interior_design.each {|x, y| puts "#{x}: #{y}" }
