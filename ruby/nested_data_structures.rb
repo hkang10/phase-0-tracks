91afae1da0d2f7969a7197194eb2911439036284
Audrey Kang <hkang10@gmail.com>
2016-05-12 20:17:32 -0500

    Access nested data structure

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -34,3 +34,32 @@ six_flags = {
         restaurants: ["Primo's Pizzeria", "Antoine's", "Go Fresh Cafe"]
       },
 }
+
+#Release 2: Accessing nested items
+puts "How many rollercoasters are in Orleans Place?"
+p six_flags[:orleans_place][:num_of_rollercoaster]
+puts ""
+
+puts "Please remove the closed restaurant 'Fiesta Fries Cantina' from Southwest Territory."
+six_flags[:southwest_territory][:restaurants].delete("Fiesta Fries Cantina")
+puts""
+
+puts "Your new list of restaurants in Southwest Territory is: "
+p six_flags[:southwest_territory][:restaurants]
+puts""
+
+puts "Please add 'Fiesta Fries Cantina' to Yankee Harbor."
+six_flags[:yankee_harbor][:restaurants].push("Fiesta Fries Cantina")
+puts ""
+
+puts "Your new list of resturants in Yankee Harbor is: "
+p six_flags[:yankee_harbor][:restaurants]
+puts ""
+
+puts "What rollercoasters are in Orleans Place?"
+p six_flags[:orleans_place][:list_of_rollercoaster]
+puts ""
+
+puts "What is the first listed rollercoaster ride in Yankee Harbor?"
+p six_flags[:yankee_harbor][:list_of_rollercoaster].first
+puts ""
