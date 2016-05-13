442a3827d0f7d8ab57e6b6a450d4d0d78afa4c1d
Audrey Kang <hkang10@gmail.com>
2016-05-12 20:16:58 -0500

    Create nested data structure to file

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,36 @@
+#Release 2: Design and Build a Nested Data Structure #Build a nested data structure
+six_flags = {
+  yankee_harbor: {
+    num_of_rollercoaster: 5,
+    list_of_rollercoaster: {
+      "Vertical Velocity" => "Flashpass",
+      "Yankee Clipper" => "Flashpass",
+      "Whrirligig" => "No Flashpass",
+      "Batman: The Ride" => "Flashpass",
+      "East River Crawler" => "No Flashpass"
+      },
+    restaurants: ["Funnel Cakes Express", "Angelo's Pasta & Pizza", " Jack's Snacks"]
+    },
+  southwest_territory: {
+      num_of_rollercoaster: 6,
+      list_of_rollercoaster: {
+        "Viper" => "Flashpass",
+        "Chubasco" => "No Flashpass",
+        "Ricochet" => "No Flashpass",
+        "Giant Drop" => "Flashpass",
+        "Raging Bull" => "Flashpass",
+        "River Rocker" => "No Flashpass"
+        },
+      restaurants: ["Fiesta Fries Cantina", "Waterin' Hole", "Cold Stone Creamery", "The Oasis"]
+      },
+  orleans_place: {
+    num_of_rollercoaster: 4,
+    list_of_rollercoaster: {
+        "The Dark Knight" => "Flashpass",
+        "Superman" => "Flashpass",
+        "Condor" => "No Flashpass",
+        "Rue Le Dodge" => "Flashpass"
+        },
+        restaurants: ["Primo's Pizzeria", "Antoine's", "Go Fresh Cafe"]
+      },
+}
