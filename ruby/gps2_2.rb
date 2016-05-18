33c6489bf78df016da600254d4630e0abd89b473
Audrey Kang <hkang10@gmail.com>
2016-05-18 00:41:19 -0500

    Create a list method

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -5,6 +5,15 @@
   # set default quantity
   # print the list to the console [can you use one of your other methods here?]
 # output: [what data type goes here, array or hash?]
+def create_list(list, grocery_hash={})
+	list_array = list.split(" ")
+	quantity = 0
+	list_array.each do |item|
+		grocery_hash[item]=quantity
+	end
+	print_list(grocery_hash)
+	return grocery_hash
+end
 
 # Method to add an item to a list
 # input: item name and optional quantity
