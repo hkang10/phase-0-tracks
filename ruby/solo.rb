fd87fae5fde1ea0197ed9c63ceedf0be367c5386
Audrey Kang <hkang10@gmail.com>
2016-05-23 07:39:27 -0500

    Initial commit with pseudocode as comments

---
+++ ruby/solo.rb
@@ -0,0 +1,13 @@
+#Release 0 - Design a class
+#3 attributes
+#3 methods (one with argument)
+
+#Release 1 -Write Your Clas
+#Implement your class
+#include - att_reader, attr_accessor, and initialize
+
+#Release 2 - Use Your Class in a Program
+#User interface via command line
+#User should be allowed to create as many instances of your class
+#Prompt user for each attribute and store these class instances into an array
+#when user indicates finished creating instances, loop through an array and print out the attributes of each instances
