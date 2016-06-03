1fcc444457ab704d17b10392d051df4f0cb5d652
Audrey Kang <hkang10@gmail.com>
2016-06-03 17:24:46 -0500

    Add code to file

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -22,3 +22,94 @@ Step 4: Write a loop that prompts for user error.
         skip to print a confirmation report.
       end
 =end
+
+interior_design = {}
+
+p "What is the Client's name?"
+interior_design[:name] = gets.chomp.capitalize
+p "What is the Client's age?"
+interior_design[:age] = gets.chomp.to_i
+p "How many children does the client have?"
+interior_design[:num_of_children] = gets.chomp.to_i
+p "Choose your decor theme. (Modern, Contemporary, Rustic, Country)"
+interior_design[:decor_theme] = gets.chomp.capitalize
+p "What is your budget?"
+interior_design[:budget] = gets.chomp.to_i
+
+p ""
+p "------------------"
+p "Client Information"
+p "------------------"
+interior_design.each do |category, user_info|
+  puts "#{category}:  #{user_info}"
+end
+
+p ""
+p "What information would you like to update?"
+p "(name, age, children, theme, budget)"
+p "or else type 'done'."
+update = gets.chomp.downcase
+if update == "name"
+  p "What is the Client's name?"
+  new_name = gets.chomp.capitalize
+  interior_design[:name] = new_name
+  p ""
+  p "------------------"
+  p "Client Information"
+  p "------------------"
+  interior_design.each do |category, user_info|
+    puts "#{category}:  #{user_info}"
+  end
+elsif update == "age"
+  p "What is the Client's age?"
+  new_age = gets.chomp.to_i
+  interior_design[:age] = new_name
+  p ""
+  p "------------------"
+  p "Client Information"
+  p "------------------"
+  interior_design.each do |category, user_info|
+    puts "#{category}:  #{user_info}"
+  end
+elsif update == "children"
+  p "How many children does the Client have?"
+  new_num_of_children = gets.chomp.to_i
+  interior_design[:num_of_children] = new_num_of_children
+  p ""
+  p "------------------"
+  p "Client Information"
+  p "------------------"
+  interior_design.each do |category, user_info|
+    puts "#{category}:  #{user_info}"
+  end
+elsif update == "theme"
+  p "Choose your decor theme. (Modern, Contemporary, Rustic, Country)"
+  new_decor_theme = gets.chomp.capitalize
+  interior_design[:decor_theme] = new_decor_theme
+  p ""
+  p "------------------"
+  p "Client Information"
+  p "------------------"
+  interior_design.each do |category, user_info|
+    puts "#{category}:  #{user_info}"
+  end
+elsif update == "budget"
+  p "What is your budget?"
+  new_budget = gets.chomp.to_f
+  interior_design[:budget] = new_budget
+  p ""
+  p "------------------"
+  p "Client Information"
+  p "------------------"
+  interior_design.each do |category, user_info|
+    puts "#{category}:  #{user_info}"
+  end
+else update == "done"
+  p ""
+  p "------------------"
+  p "Client Information"
+  p "------------------"
+  interior_design.each do |category, user_info|
+    puts "#{category}:  #{user_info}"
+  end
+end
