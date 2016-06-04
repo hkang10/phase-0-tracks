41d782579eae58d03bbd0379fb9e9428a750b5a0
Audrey Kang <hkang10@gmail.com>
2016-06-03 19:59:16 -0500

    add release 2 to file

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -41,3 +41,41 @@ p "*** New list of freezer items ***"
 freezer.each do |food, day|
   puts "#{food} on #{day}"
 end
+
+#Release 2
+array = [ 1, 2, 3, 4, 5]
+
+hash = {
+  "a" => 1,
+  "b" => 2,
+  "c" => 3,
+  "d" => 4,
+  "e" => 5
+}
+#A method that iterates through the items, deleting any that meet a certain condition (for example, deleting any numbers that are less than 5)
+array.delete_if {|num| num < 3}
+p array
+
+hash.delete_if{|x, y| y < 2}
+puts hash
+
+#A method that filters a data structure for only items that do satisfy a certain condition (for example, keeping any numbers that are less than 5)
+array.keep_if {|num| num > 3}
+p array
+
+hash.keep_if{|x, y| y < 3}
+puts hash
+
+#A different method that filters a data structure for only items satisfying a certain condition -- Ruby offers several options!
+array.select! { |num| num.even? }
+p array
+
+hash.select!{|x, y| y > 2}
+puts hash
+
+#A method that will remove items from a data structure until the condition in the block evaluates to false, then stops
+array.reject! {|num| num > 2}
+p array
+
+hash.reject!{|x, y| y > 3}
+puts hash
