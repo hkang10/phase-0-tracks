03106a885222d989c3a2d7e33c5db89b1168ae6e
Audrey Kang <hkang10@gmail.com>
2016-06-29 16:31:39 -0500

    Add directory and files to pairing project

---
+++ web_dev/sinatra_templating/app.rb
@@ -0,0 +1,44 @@
+# require gems
+require 'sinatra'
+require 'sqlite3'
+
+set :public_folder, File.dirname(__FILE__) + '/static'
+
+db = SQLite3::Database.new("students.db")
+db.results_as_hash = true
+
+# show students on the home page
+get '/' do
+  @students = db.execute("SELECT * FROM students")
+  erb :home
+end
+
+get '/students/new' do
+  erb :new_student
+end
+
+get '/students/remove' do
+	erb :remove_student
+end
+
+get '/students/update' do
+	erb :update_student
+end
+# create new students via
+# a form
+post '/students' do
+  db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
+  redirect '/'
+end
+
+post '/students/remove' do
+  db.execute("DELETE FROM students WHERE name=?", [params['remove']]);
+  redirect '/'
+end
+
+post '/students/update' do
+  db.execute("UPDATE students SET campus=? WHERE name=?",params['updateCampus'],params['Name'])
+  db.execute("UPDATE students SET age=? WHERE name=?",params['updateAge'],params['Name'])
+  redirect '/'
+end
+# add static resources
